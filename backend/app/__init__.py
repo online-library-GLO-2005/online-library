d5f9983dd46e2c2d828460ebc79e402cb6775b33
Nicolas Veiga <nivec@ulaval.ca>
2026-03-24 17:54:00 -0400

    refactor: init of app cleaner

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from app.utils.errors import AppError
+from app.config import Config
+
 from app.extensions import ma, jwt
 from app.routes.health import bp as health_bp
 from app.routes.books import bp as books_bp
-from app.utils.errors import AppError
 
-# Config class
-from app.config import Config
 
-def create_app():
+def register_blueprints(app: Flask) -> None:
+    app.register_blueprint(health_bp)
+    app.register_blueprint(books_bp)
+
+
+def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(Config)
 
@@ -15,9 +20,7 @@ def create_app():
     ma.init_app(app)
     jwt.init_app(app)
 
-    # Register blueprints
-    app.register_blueprint(health_bp)
-    app.register_blueprint(books_bp)
+    register_blueprints(app)
 
     # Global error handler
     @app.errorhandler(AppError)
@@ -25,9 +28,3 @@ def create_app():
         return {"error": e.__class__.__name__, "message": e.message}, e.status_code
 
     return app
-
-
-# Only run if executed directly
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
